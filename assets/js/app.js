90653e292f6bf2ac9c9565e80d928db6d2400856
serginhohalves <80188778+serginhohalves@users.noreply.github.com>
2022-08-10 22:11:46 -0300

    backdrop and close menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
 // efect canvas menu
-const btnCanvas = document.querySelector('.canvas'); 
-const menuCanvas = document.querySelector('.off-canvas-menu');
-const close = document.querySelector('.close');
+const openCanvas = document.querySelector(".canvas");
+const menuCanvas = document.querySelector(".off-canvas-menu");
+const closeCanvas = document.querySelector(".close");
+const backdrop = document.querySelector(".backdrop");
 
-btnCanvas.addEventListener('click', toogleCanvas);
-function toogleCanvas(){
-    menuCanvas.classList.toggle('is-active');
+function toogleCanvas() {
+  menuCanvas.classList.toggle("is-active");
+  backdrop.classList.toggle("hidden");
 }
 
+closeCanvas.addEventListener("click", toogleCanvas);
+openCanvas.addEventListener("click", toogleCanvas);
